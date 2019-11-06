df881fc7a7550cf2dfc9715e8f5ba8d87a0f0415
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-11-06 10:49:19 -0500

    Chuck Norris doesn’t wash his clothes, he disembowels them.

--- serve.rkt
+++ serve.rkt
@@ -1,35 +1,106 @@
 #lang racket
+(require xml net/url)
 
-(define (go)
-  'yep-it-works)
+; (define stop (serve 8080))
 
 (define (serve port-no)
-  (define listener (tcp-listen port-no 5 #t))
-  (define (loop)
-    (accept-and-handle listener)
-    (loop))
-  (define t (thread loop))
+  (define main-cust (make-custodian))
+  (parameterize ([current-custodian main-cust])
+    (define listener (tcp-listen port-no 5 #t))
+    (define (loop)
+      (accept-and-handle listener)
+      (loop))
+    (thread loop))
   (lambda ()
-    (kill-thread t)
-    (tcp-close listener)))
+    (custodian-shutdown-all main-cust)))
 
 (define (accept-and-handle listener)
-  (define-values (in out) (tcp-accept listener))
-  (define t (thread
-             (lambda ()
-               (handle in out)
-               (close-input-port in)
-               (close-output-port out))))
-  ; Watcher thread
+  (define cust (make-custodian))
+  (custodian-limit-memory cust (* 50 1024 1024))
+  (parameterize ([current-custodian cust])
+    (define-values (in out) (tcp-accept listener))
+    (thread (lambda ()
+              (handle in out)
+              (close-input-port in)
+              (close-output-port out))))
+  ; Watcher thread:
   (thread (lambda ()
             (sleep 10)
-            (kill-thread t))))
+            (custodian-shutdown-all cust))))
 
 (define (handle in out)
-  ; Discard the request header (up to blank line)
-  (regexp-match #rx"(\r\n|^)\r\n" in)
-  ; Send reply:
-  (display "HTTP/1.0 200 Okay\r\n" out)
-  (display "Server: k\r\nContent-Type: text/html\n\r\n" out)
-  (display "<html><body>Hello, world!</body></html>" out))
+  (define req
+    ; Match the first line to extract the request:
+    (regexp-match #rx"^GET (.+) HTTP/[0-9]+\\.[0-9]+"
+                  (read-line in)))
+  (when req
+    ; Discard the rest of the header (up to blank line):
+    (regexp-match #rx"(\r\n|^)\r\n" in)
+    ; Dispatch:
+    (let ([xexpr (dispatch (list-ref req 1))])
+      ; Send reply:
+      (display "HTTP/1.0 200 Okay\r\n" out)
+      (display "Server: k\r\nContent-Type: text/html\r\n\r\n" out)
+      (display (xexpr->string xexpr) out))))
+
+(define (dispatch str-path)
+  ; Parse the request as a URL
+  (define url (string->url str-path))
+  (define path (map path/param-path (url-path url)))
+  ; Find a handler based on the path's first element:
+  (define h (hash-ref dispatch-table (car path) #f))
+  (if h
+      ; Call a handler
+      (h (url-query url))
+      ; No handler found
+      `(html (head (title "Error"))
+             (body
+              (font ((color "red"))
+                    ,str-path)))))
+
+(hash-set! dispatch-table "hello"
+           (lambda (query)
+             `(html (body "Hello, world!"))))
+
+(define (build-request-page label next-url hidden)
+  `(html
+    (head (title "Enter a Number to Add"))
+    (body ([bgcolor "white"])
+          (form ([action ,next-url] [method "get"])
+                ,label
+                (input ([type "text"] [name "number"]
+                                      [value ""]))
+                (input ([type "hidden"] [name "hidden"]
+                                        [value ,hidden]))
+                (input ([type "submit"] [name "enter"]
+                                        [value "Enter"]))))))
+
+(define (many query)
+  (build-request-page "Number of greetings:" "/reply" ""))
+
+(define (reply query)
+  (define n (string->number (cdr (assq 'number query))))
+  `(html (body ,@(for/list ([i (in-range n)])
+                   " hello"))))
+
+(define (sum query)
+  (build-request-page "First Number:" "/one" ""))
+
+(define (one query)
+  (build-request-page "Second Number:"
+                      "two"
+                      (cdr (assq 'number query))))
+
+(define (two query)
+  (let ([n (string->number (cdr (assq 'hidden query))]
+           [m (string->number (cdr (assq 'number query)))])
+        `(html (body "The sum is " ,(number->string (+ m n)))))))
+
+(define dispatch-table (make-hash))
+(hash-set! dispatch-table "many" many)
+(hash-set! dispatch-table "reply" reply)
+(hash-set! dispatch-table "sum" sum)
+(hash-set! dispatch-table "one" one)
+(hash-set! dispatch-table "two" two)
+
 
